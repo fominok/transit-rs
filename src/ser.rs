39c8f08a08691665102efaafff84a0c6c209c96e
Evgeny Fomin <fominok@hotmail.com>
2019-04-01 06:47:52 +0300

    strange

--- src/ser.rs
+++ src/ser.rs
@@ -1,26 +1,75 @@
+use serde_json::{Value as JsVal, json};
 use std::collections::BTreeMap;
-use serde_json::Value as JsVal;
 
-trait TfSerialize {}
+fn to_transit_json<T: TfSerialize>(v: T) -> JsVal {
+    v.transit_serialize()
+}
 
+trait TfSerialize {
+    fn transit_serialize(&self) -> JsVal;
+}
 
-// Use this later for generic tagged val serilization
-// impl<T: std::fmt::Debug> TfSerializeKey for T {
-//     fn serialize_key(&self) -> String {
-//         format!("{:?}", self)
-//     }
-// }
+enum TfType {
+    Scalar,
+    Composite,
+}
 
-// trait TfSerializeMap {
-//     fn serialize_map<K, V>
-// }
+trait HasTfType {
+    fn get_transit_type(&self) -> TfType;
+}
+
+macro_rules! impl_comp {
+    ($name:ty, $($type_params:tt),+) => {
+        impl<$($type_params,)+> HasTfType for $name {
+            fn get_transit_type(&self) -> TfType {
+                TfType::Composite
+            }
+        }
+    };
+    ($name:ty) => {
+        impl HasTfType for $name {
+            fn get_transit_type(&self) -> TfType {
+                TfType::Composite
+            }
+        }
+    };
+}
+
+macro_rules! impl_scalar {
+    ($name:ty) => {
+        impl HasTfType for $name {
+            fn get_transit_type(&self) -> TfType {
+                TfType::Scalar
+            }
+        }
+    };
+}
+
+impl_scalar!(i32);
+impl_comp!(BTreeMap<K,V>, K, V);
+
+impl<K: HasTfType, V: TfSerialize> TfSerialize for BTreeMap<K, V> {
+    fn transit_serialize(&self) -> JsVal {
+
+    }
+}
+
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_serialize_key() {
-        // assert_eq!("5", 5_i32.serialize_key());
+    fn test_scalar_map() {
+        let mut m = BTreeMap::new();
+        m.insert(4, "yolo");
+        m.insert(-6, "swag");
+
+        let tr = to_transit_json(m);
+        assert_eq!(json!({
+            "~i4": "yolo",
+            "~i-6": "swag"
+        }), tr);
+
     }
 }
